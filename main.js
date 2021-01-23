95319e552e1e451ca8adb476a11565e5669a5ed3
Riccardo <riccardo.contigiani393@gmail.com>
2021-01-23 18:10:09 +0100

    feat: :sparkles: message by hour

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const lineReader = require("line-reader");
 var parse = require("date-fns/parse");
+var getHours = require("date-fns/getHours");
 var _ = require("lodash");
 var onlyEmoji = require("emoji-aware").onlyEmoji;
 
@@ -43,37 +44,81 @@ const readChat = () => {
 };
 
 const analyzeChat = () => {
-  const chatGroupedBySender = getGroupedBySender();
-  /*const nMessageBySender = getNMessageBySender(chatGroupedBySender);
+  const chatGroupedBySender = getGroupedBySender(chat);
+  const nMessageBySender = getNMessageBySender(chatGroupedBySender);
   const messageLettersAvgLengthBySender = getMessageLettersAvgLengthBySender(
     chatGroupedBySender
   );
   const messageWordsAvgLengthBySender = getMessageWordsAvgLengthBySender(
     chatGroupedBySender
-  );*/
+  );
+
+  const messageByHours = getMessageByHours();
 
   const emojiMessageGroupedBySender = getEmojiMessageBySender(
     chatGroupedBySender
   );
-  /*const messageNEmojiTotalUsageBySender = getMessageNEmojiTotalUsageBySender(
+  const messageNEmojiTotalUsageBySender = getMessageNEmojiTotalUsageBySender(
     emojiMessageGroupedBySender
-  );*/
+  );
   const mostUsedEmojiBySender = getMostUsedEmojiBySender(
     emojiMessageGroupedBySender
   );
 
   results = {
-    //nMessageBySender: nMessageBySender,
-    //messageLettersAvgLengthBySender: messageLettersAvgLengthBySender,
-    //messageWordsAvgLengthBySender: messageWordsAvgLengthBySender,
-    //messageNEmojiTotalUsageBySender: messageNEmojiTotalUsageBySender,
+    nMessageBySender: nMessageBySender,
+    messageLettersAvgLengthBySender: messageLettersAvgLengthBySender,
+    messageWordsAvgLengthBySender: messageWordsAvgLengthBySender,
+    messageNEmojiTotalUsageBySender: messageNEmojiTotalUsageBySender,
     mostUsedEmojiBySender: mostUsedEmojiBySender,
+    messageByHours: messageByHours,
   };
   writeResultFile();
 };
 
-const getGroupedBySender = () => {
-  return _.groupBy(chat, "msgSender");
+const getGroupedBySender = (items) => {
+  return _.groupBy(items, "msgSender");
+};
+
+const getMessageByHours = () => {
+  var kpi = {};
+  try {
+    const hourMessageList = [];
+    for (i = 1; i < 25; i++) {
+      const hourItem = chat.filter((m) => getHours(m.date) === i);
+      const item = {
+        hour: i,
+        items: hourItem,
+      };
+      hourMessageList.push(item);
+    }
+
+    const hourMessageListGroupedBySender = [];
+    hourMessageList.forEach((hi) => {
+      const items = hi?.items;
+      const grouped = getGroupedBySender(items);
+      const hourItem = {};
+      let sendersTotal = 0;
+      Object.entries(grouped).forEach(([key, value]) => {
+        const len = value?.length;
+        hourItem[key] = len;
+        sendersTotal += len;
+      });
+
+      hourItem.total = sendersTotal;
+
+      const item = {
+        hour: hi?.hour,
+        count: hourItem,
+      };
+      hourMessageListGroupedBySender.push(item);
+    });
+
+    kpi = hourMessageListGroupedBySender;
+  } catch (e) {
+    console.error(e);
+  }
+  return kpi;
 };
 
 const getEmojiMessageBySender = (groupedSet) => {
@@ -156,7 +201,7 @@ const getMostUsedEmojiBySender = (groupedSet) => {
         key: key,
         value: value,
       }));
-      const orderedEmoji = _.orderBy(count, ['value'],['desc']);
+      const orderedEmoji = _.orderBy(count, ["value"], ["desc"]);
       kpi[sender] = orderedEmoji;
     } catch (e) {
       console.error(e);
